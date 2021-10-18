f0308f74c020b8f72a5cd174773ad3ece5c73c51
kb5ux <kb5ux@virginia.edu>
2021-10-18 15:18:30 -0400

    Updated Login Tests

--- housing/tests.py
+++ housing/tests.py
@@ -1,25 +1,45 @@
-from django.contrib.auth import authenticate
+from __future__ import absolute_import, unicode_literals
+from allauth.socialaccount.providers.google.provider import GoogleProvider
+from allauth.socialaccount.tests import OAuth2TestsMixin
+from allauth.tests import MockedResponse
 from django.contrib.auth.models import User
-from django.test import TestCase, Client
+from django.test import TestCase
 
 
 # Create your tests here.
+class GoogleLoginTests(OAuth2TestsMixin, TestCase):
+    provider_id = GoogleProvider.id
 
-class GoogleLogin(TestCase):
-    def test_ValidUser(self):
-        self.c = Client()
-        self.user = User.objects.create(username='test', password='123')
-        self.user.set_password('456')
-        self.user.save()
-        self.user = authenticate(username='test', password='456')
-        login = self.c.login(username='test', password='456')
-        self.assertTrue(login)
+    def get_mocked_response(self, last_name="Case", first_name="Test",
+                            name="Test Case", email="testcase@test.com",
+                            verified=True):
+        return MockedResponse(200, """{"last_name": "%s", "first_name": "%s", "name": "%s", 
+                                        "email": "%s", "id": "1",
+                                        "verified": %s }
+                                    """
+                              % (
+                                  last_name,
+                                  first_name,
+                                  name,
+                                  email,
+                                  (repr(verified).lower()),
+                              )
+                              )
 
-    def test_InvalidUser(self):
-        self.c = Client()
-        self.user = User.objects.create(username='test', password='123')
-        self.user.set_password('456')
-        self.user.save()
-        self.user = authenticate(username='test', password='456')
-        login = self.c.login(username='test', password='123')
-        self.assertFalse(login)
+    def test_Email_WrongUsername(self):
+        first_name = "John"
+        last_name = "Smith"
+        email = "testcase@test.com"
+        self.login(self.get_mocked_response(name=first_name + " " + last_name, email=email,
+                                            first_name=first_name, last_name=last_name, verified=True))
+        test_user = User.objects.get(email=email)
+        self.assertEqual(test_user.username, "testcase")
+
+    def test_Email_WrongEmail(self):
+        first_name = "John"
+        last_name = "Smith"
+        email = "testcases@test.com"
+        self.login(self.get_mocked_response(name=first_name + " " + last_name, email=email,
+                                            first_name=first_name, last_name=last_name, verified=True))
+        test_user = User.objects.get(email=email)
+        self.assertNotEqual(test_user.username, "testcase")
