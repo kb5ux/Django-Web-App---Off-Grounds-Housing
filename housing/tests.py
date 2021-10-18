9bbe368499167c2ad5c10f487e645b9e5e4a436c
kb5ux <kb5ux@virginia.edu>
2021-10-17 20:46:05 -0400

    Login Test Case

--- housing/tests.py
+++ housing/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+
+class GoogleLogin(TestCase):
+    def test_ValidUser(self):
+        self.c = Client()
+        self.user = User.objects.create(username='test', password='123')
+        self.user.set_password('456')
+        self.user.save()
+        self.user = authenticate(username='test', password='456')
+        login = self.c.login(username='test', password='456')
+        self.assertTrue(login)
+
+    def test_InvalidUser(self):
+        self.c = Client()
+        self.user = User.objects.create(username='test', password='123')
+        self.user.set_password('456')
+        self.user.save()
+        self.user = authenticate(username='test', password='456')
+        login = self.c.login(username='test', password='123')
+        self.assertFalse(login)
