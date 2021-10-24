4e54bd94ad325643239b4c0214375fbe22a2bb7f
kb5ux <kb5ux@virginia.edu>
2021-10-24 19:09:02 -0400

    Starting Models

--- housing/models.py
+++ housing/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Housing(models.Model):
+    title = models.CharField(max_length=200)
+    street_address = models.CharField(max_length=150)
+    city = models.CharField(max_length=75)
+    state = models.CharField(max_length=75)
+    zipcode = models.CharField(max_length=25)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)  # Can have 1.5 baths
+    square_feet = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    listing_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=200)
+    listing_id = models.IntegerField()
+    review_description = models.TextField(blank=True)
+    review_score = models.DecimalField(max_digits=2, decimal_places=1)
+    review_date = models.DateTimeField(default=datetime.now, blank=True)
