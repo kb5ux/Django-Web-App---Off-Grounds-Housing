174c6e861bd948825115b13523c86299720bbab8
smithlauren785 <89806439+smithlauren785@users.noreply.github.com>
2021-10-31 13:16:49 -0400

    Added buttons connecting the listing and home pages.

--- housing/admin.py
+++ housing/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Housing
 
-# Register your models here.
+'''
+
+
 class HousingAdmin(admin.ModelAdmin):
    fields = ('title','street_address','city','state','zipcode','description','price','bedrooms','bathrooms',
                     'square_feet','is_available','listing_date')
 
-admin.site.register(Housing,HousingAdmin)
+'''
+# Register your models here.
+
+admin.site.register(Housing)
--- housing/views.py
+++ housing/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Housing, Review
 from django.views import generic
 
-
 def index(request):
-    return HttpResponse("Welcome to the Off-grounds Housing Website!")
+  return render(request, 'main_page.html')
 
+
+# Class-based list view for the Housing model
 
 class ListingListView(generic.ListView):
     model = Housing
     template_name = 'housing.html'
+    context_object_name = 'listings'
+
+    def get_queryset(self):
+        return Housing.objects.all()
 
 
+
+
+    '''
+    A method for it we want to post information for a listing that is not from the admin webiste
+       - only included the street_address part of it here
+    
+
+    def post(request):
+        street_address = request.POST.get('street address')
+        #if not(street_address):
+            #return HttpResponseRedirect(reverse('housing'))
+        try:
+            one_listing = Housing(street_address = street_address)
+            one_listing.save()
+        except(KeyError, Housing.DoesNotExist):
+            return render(request, 'housing.html', {
+              'error_message': "You didn't enter a listing."})
+        return HttpResponseRedirect(reverse('housing:listing'))
+'''
