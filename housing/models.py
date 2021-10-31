173d8cc0062153bb02b4bd87618a2609ba840ca7
kb5ux <kb5ux@virginia.edu>
2021-10-31 12:52:10 -0400

    Commit

--- housing/models.py
+++ housing/models.py
@@ -14,6 +14,7 @@ class Housing(models.Model):
     bedrooms = models.IntegerField()
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)  # Can have 1.5 baths
     square_feet = models.IntegerField()
+    demo_image = models.ImageField()
     is_available = models.BooleanField(default=True)
     listing_date = models.DateTimeField(default=datetime.now, blank=True)
 
@@ -27,3 +28,15 @@ class Review(models.Model):
     review_description = models.TextField(blank=True)
     review_score = models.DecimalField(max_digits=2, decimal_places=1)
     review_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class RentalCompany(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
