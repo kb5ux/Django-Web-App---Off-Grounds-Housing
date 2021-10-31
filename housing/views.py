921e83e8198befaa631e3cc0a9b0f88cb9929243
kb5ux <kb5ux@virginia.edu>
2021-10-31 16:46:31 -0400

    Search Feature

--- housing/views.py
+++ housing/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -5,11 +7,38 @@ from django.urls import reverse
 from .models import Housing, Review
 from django.views import generic
 
-def index(request):
-  return render(request, 'main_page.html')
 
 
-# Class-based list view for the Housing model
+def main_page(request):
+    return render(request, 'main_page.html')
+
+
+def search_results(request):
+    queryset_list = Housing.objects.order_by('-listing_date')
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    if 'bathrooms' in request.GET:
+        bathrooms = request.GET['bathrooms']
+        if bathrooms:
+            queryset_list = queryset_list.filter(bathrooms__lte=bathrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'listings': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'search_results.html', context)
+
+
 
 class ListingListView(generic.ListView):
     model = Housing
