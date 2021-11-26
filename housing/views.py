a5d105e4f621a665e0842dc90c895a3be2e08194
kb5ux <89873190+kb5ux@users.noreply.github.com>
2021-11-25 19:51:13 -0500

    Update views.py

--- housing/views.py
+++ housing/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.db.models import Avg
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
